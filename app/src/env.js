9732607ef9c796179ae63a234efee1e167230fe5
Antonios <antwnis@gmail.com>
2016-07-04 19:15:52 +0100

    Materialize -up the ui

--- app/src/env.js
+++ app/src/env.js
@@ -1,4 +1,4 @@
 var ENV = {
-  SCHEMA_REGISTRY: "http://localhost:8081"
-  //SCHEMA_REGISTRY: "http://schema_registry.landoop.com:28081"
+  //SCHEMA_REGISTRY: "http://localhost:8081"
+  SCHEMA_REGISTRY: "http://schema_registry.landoop.com:28081"
 };
