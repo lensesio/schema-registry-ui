175467b257f55e10165baa2956a42d94edfec659
Marios Andreopoulos <opensource@andmarios.com>
2017-01-07 18:17:32 +0200

    Docker (fix): update run.sh for new Schema Registry UI configuration layout.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- docker/run.sh
+++ docker/run.sh
@@ -15,13 +15,16 @@ if [[ -z "$SCHEMAREGISTRY_URL" ]]; then
     echo "Schema Registry URL was not set via SCHEMAREGISTRY_URL environment variable."
 else
     echo "Setting Schema Registry URL to $SCHEMAREGISTRY_URL."
-    sed -e 's|^\s*var SCHEMA_REGISTRY =.*|  var SCHEMA_REGISTRY = "'"$SCHEMAREGISTRY_URL"'";|' \
-        -i /schema-registry-ui/combined.js
+    cat <<EOF >schema-registry-ui/env.js
+var clusters = [
+   {
+     NAME: "default",
+     SCHEMA_REGISTRY: "$SCHEMAREGISTRY_URL"
+   }
+]
+EOF
 fi
 
-# echo "Final configuration is:"
-# echo
-# cat /schema-registry-ui/combined.js
-# echo
+echo
 
 exec /caddy/caddy -conf /caddy/Caddyfile
