cf14405eb191466bdb1aa822a0988d70aa07d974
Giannis <jglambed@gmail.com>
2017-03-10 23:16:52 +0200

    transitive compatibility for 3.1.2 and above

--- env.js
+++ env.js
@@ -3,15 +3,13 @@ var clusters = [
        NAME:"prod",
        // Schema Registry service URL (i.e. http://localhost:8081)
        SCHEMA_REGISTRY: "http://localhost:8081", // https://schema-registry.demo.landoop.com
-       SCHEMA_REGISTRY_VERSION: "3.0.1", // Declaring schema registry version
        COLOR: "#141414" // optional
      },
      {
        NAME:"dev",
        SCHEMA_REGISTRY: "http://localhost:8383",
-       SCHEMA_REGISTRY_VERSION: "3.1.0", // Declaring schema registry version
        COLOR: "red", // optional
        allowGlobalConfigChanges: true, // optional
-       allowTransitiveCompatibilities: true        // if using a Confluent Platform release >= 3.2.0 uncomment this line
+       //allowTransitiveCompatibilities: true        // if using a Confluent Platform release >= 3.1.2 uncomment this line
      }
   ];
