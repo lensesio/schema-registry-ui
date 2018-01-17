33aa1c3a35ebd56502b406ed039e46f5d401e59c
jglambed <jglambed@gmail.com>
2018-01-17 02:23:45 +0200

    fix webpack font issue

--- webpack.config.js
+++ webpack.config.js
@@ -46,15 +46,15 @@ const config = {
 
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+                loader: "url-loader?limit=10000&mimetype=application/font-woff&outputPath=&publicPath=/"
             },
             {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
+                loader: "file-loader&outputPath=&publicPath=/"
             },
             {
                 test: /\.html$/,
-                loader: "html-loader"
+                loader: "html-loader&outputPath=&publicPath=/"
             }
         ]
     },
